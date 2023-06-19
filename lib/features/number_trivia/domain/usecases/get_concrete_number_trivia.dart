e1d340273d977797e286d1c8edb55872801bdd86
sakshipore <88977771+sakshipore@users.noreply.github.com>
2023-06-19 21:06:18 +0530

    Entities and Usecases

---
+++ lib/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart
@@ -0,0 +1,15 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_tdd_clean_architecture/features/number_trivia/domain/entities/number_trivia.dart';
+import 'package:flutter_tdd_clean_architecture/features/number_trivia/domain/repositories/number_trivia_repository.dart';
+
+import '../../../../core/error/failure.dart';
+
+class GetConcreteNumberTrivia {
+  final NumberTriviaRepository repository;
+
+  GetConcreteNumberTrivia({required this.repository});
+
+  Future<Either<Failure, NumberTrivia>> execute({required int number}) async {
+    return await repository.getConcreteNumberTrivia(number);
+  }
+}
